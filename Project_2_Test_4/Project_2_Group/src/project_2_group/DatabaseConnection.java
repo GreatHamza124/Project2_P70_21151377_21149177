c0ec7b56ca4a5b8b7efab6e3c18a360ba41b3f19
Hamza Hussain <wct8670@autuni.ac.nz>
2023-10-29 20:04:31 +1300

    Hamza branch - 2nd Commit
    Hamza branch - 2nd Commit

--- Project_2_Test_4/Project_2_Group/src/project_2_group/DatabaseConnection.java
+++ Project_2_Test_4/Project_2_Group/src/project_2_group/DatabaseConnection.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package project_2_group_copy_project;
+package project_2_group;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
